d730cce962300e706078f69647fb2900ed5d408e
tanselbay1 <tanselbay1@hotmail.com>
2022-11-07 14:28:48 +0700

    Make the app responsive

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,20 +1,22 @@
 const Result = ({ tipAmount, totalAmount, onReset, isTyping }) => {
   return (
     <div className="result">
-      <div className="tip-amount">
-        <div className="tip-amount__text">
-          <h3 className="result__header">Tip Amount</h3>
-          <p className="result__text">/ person</p>
+      <div className="results">
+        <div className="tip-amount">
+          <div className="tip-amount__text">
+            <h3 className="result__header">Tip Amount</h3>
+            <p className="result__text">/ person</p>
+          </div>
+          <p className="amount__usd">${tipAmount}</p>
+          {/* {tipAmount && "$0.00"} */}
         </div>
-        <p className="amount__usd">${tipAmount}</p>
-        {/* {tipAmount && "$0.00"} */}
-      </div>
-      <div className="total-amount">
-        <div className="total-amount__text">
-          <h3 className="result__header">Total</h3>
-          <p className="result__text">/ person</p>
+        <div className="total-amount">
+          <div className="total-amount__text">
+            <h3 className="result__header">Total</h3>
+            <p className="result__text">/ person</p>
+          </div>
+          <p className="amount__usd">${totalAmount}</p>
         </div>
-        <p className="amount__usd">${totalAmount}</p>
       </div>
       <button
         type="reset"
